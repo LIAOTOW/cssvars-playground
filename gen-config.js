6f2675caa04c778feea5154e97a87a62fa0b34ac
liaoliao <1030219089a@gmail.com>
2020-04-27 23:19:44 +0800

    Add tailwind transform to cssvars

---
+++ gen-config.js
@@ -0,0 +1,79 @@
+const cloneDeep = require('lodash/cloneDeep')
+const tailwindDefault = require('tailwindcss/defaultTheme')
+
+module.exports = function (options = {}, varModules = {
+  colors: 'color',
+  fontSize: 'text',
+  lineHeight: 'leading',
+  letterSpacing: 'tracking',
+  backgroundSize: 'bg',
+  borderWidth: 'border',
+  borderRadius: 'rounded',
+  width: 'w',
+  height: 'h',
+  minWidth: 'min-w',
+  minHeight: 'min-h',
+  maxWidth: 'max-w',
+  maxHeight: 'max-h',
+  // padding: 'p',
+  // margin: 'm',
+  boxShadow: 'shadows',
+  zIndex: 'z',
+  opacity: 'opacity',
+  // screens: '',
+  // fontWeight: 'font',
+}) {
+  const root = {}
+  const mergedConfig = merge(tailwindDefault, options)
+  const varsConfig = cloneDeep(mergedConfig)
+
+  Object.keys(varModules).forEach(moduleKey => {
+    const moduleName = varModules[moduleKey]
+    const config = varsConfig[moduleKey]
+
+    for (let key in config) {
+      const val = config[key]
+      if (typeof val === 'string') {
+        let varName = `--${moduleName}-${key}`.replace(/-default$/, '')
+        root[varName] = val
+        varsConfig[moduleKey][key] = `var(${varName})`
+      } else if (isObject(val)) {
+
+        for (let keykey in val) {
+          const valval = val[keykey]
+          if (typeof valval === 'string') {
+            let varName = `--${moduleName}-${key}-${keykey}`.replace(/-default$/, '')
+            root[varName] = valval
+            varsConfig[moduleKey][key][keykey] = `var(${varName})`
+          }
+        }
+      }
+    }
+  })
+
+  return {
+    root,
+    varsConfig,
+    mergedConfig,
+  }
+}
+
+const isObject = (value) => {
+  const type = typeof value
+  return value !== null && (type === 'object' || type === 'function')
+}
+
+const merge = (source, other) => {
+  if (!isObject(source) || !isObject(other)) {
+    return other === undefined ? source : other
+  }
+  // 合并两个对象的 key，另外要区分数组的初始值为 []
+  return Object.keys({
+    ...source,
+    ...other
+  }).reduce((acc, key) => {
+    // 递归合并 value
+    acc[key] = merge(source[key], other[key])
+    return acc
+  }, Array.isArray(source) ? [] : {})
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,72 @@
+const plugin = require('tailwindcss/plugin')
+const genVarsConfig = require('./gen-config')
+
+const {root, varsConfig} = genVarsConfig({
+  colors: {
+    gray: {
+      100: '#f8f8f8',
+      200: '#e3e4e6',
+      300: '#d5d5d5',
+      400: '#b1b3b8',
+      500: '#909399',
+      600: '#82848a',
+      700: '#666666',
+      800: '#595959',
+      900: '#333333',
+    },
+    red: {
+      100: '#fff5f5',
+      200: '#fed7d7',
+      300: '#feb2b2',
+      400: '#fc8181',
+      500: '#f56565',
+      600: '#e53e3e',
+      700: '#c53030',
+      800: '#9b2c2c',
+      900: '#742a2a',
+    },
+    green: {
+      100: '#f0fff4',
+      200: '#c6f6d5',
+      300: '#9ae6b4',
+      400: '#68d391',
+      500: '#48bb78',
+      600: '#38a169',
+      700: '#2f855a',
+      800: '#276749',
+      900: '#22543d',
+    },
+    blue: {
+      100: '#e8f3fe',
+      200: '#c8e2fd',
+      300: '#a7d0fc',
+      400: '#64adf9',
+      500: '#228af7',
+      600: '#1a5cbc',
+      700: '#145394',
+      800: '#0f3e6f',
+      900: '#0a294a',
+    },
+    orange: {
+      500: '#F7B522',
+    },
+  },
+  boxShadow: {
+    default: '0 0 8px 0 rgba(0, 0, 0, 0.1)',
+  },
+}, {colors: 'color'})
+
 module.exports = {
   theme: {
-    extend: {},
+    ...varsConfig,
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({addComponents}) {
+      addComponents({
+        ':root': {
+          ...root,
+        },
+      })
+    }),
+  ],
 }
